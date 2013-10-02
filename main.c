9e62415b613fd445d3fbddf528dff35e24c6c077
VincentSteil <vincent.steil@utexas.edu>
2013-10-02 23:00:24 +0100

    Update and rename OLEDTestMain.c to main.c

--- main.c
+++ main.c
@@ -1,26 +1,9 @@
-// OLEDTestMain.c
+// main.c
 // Runs on LM3S1968
-// Test Output.c by sending various characters and strings to
-// the OLED display and verifying that the output is correct.
-// Daniel Valvano
-// July 28, 2011
+// Vincent Steil && William Kelsey
 
-/* This example accompanies the book
-   "Embedded Systems: Real Time Interfacing to the Arm Cortex M3",
-   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2011
-   Section 3.4.5
 
- Copyright 2011 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
+
 
 #include <stdio.h>
 #include "output.h"
